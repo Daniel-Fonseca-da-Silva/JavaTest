b0afa8928d5276006c26170e42898a0cae11168b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-04 18:07:38 -0300

    Ambiente configuradado e testado

---
+++ src/test/java/com/projeto/SigaBem/SigaBemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.SigaBem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaBemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
